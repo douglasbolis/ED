dc200c19b0112aedd57e4df4025513cade8e6f2f
douglasbolis <douglas.lima@xdevel.com.br>
2015-12-08 22:32:51 -0200

    Iniciado o trabalho 3 de estrutura de dados.

---
+++ trabalhos/trabalho_03/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: douglas
+ *
+ * Created on 8 de Dezembro de 2015, 22:29
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
